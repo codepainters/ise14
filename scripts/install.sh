6ce7d37b5882e45a7032b8b24f7c41c0a3aefdd2
Przemyslaw Wegrzyn <pwegrzyn@codepainters.com>
2024-03-19 10:49:14 +0100

    Added libxtst6, FPGA Editor now works (#3)

--- scripts/install.sh
+++ scripts/install.sh
@@ -11,7 +11,7 @@ apt install -y --no-install-recommends \
     libncurses5 libstdc++5 \
     libx11-6 libglib2.0-0 libsm6 libxi6 libxm4 libxp6 \
     libxrender1 libxrandr2 libfreetype6 libfontconfig1 \
-    libxcursor1 libusb-0.1-4
+    libxcursor1 libxtst6 libusb-0.1-4
 
 # Install actual Xilinx software in headless mode
 yes |  /xilinx/Xilinx_ISE_DS_Lin_14.7_1015_1/bin/lin64/batchxsetup \
